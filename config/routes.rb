ffe179c6d94d4fe2ceae3ad427025757b98b660e
Danielle Rosenthal <danieller530@gmail.com>
2025-02-07 02:53:20 +0000

    adding final application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "homepage", :action => "homepage"})
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+  #get("/dice/2/6", {:controller => "dice", :action => "roll26"})
 
-  # get "/your_first_screen" => "pages#first"
-  
+  #get("/dice/2/10", {:controller => "dice", :action => "roll210"})
+
+  #get("/dice/5/4", {:controller => "dice", :action => "roll54"})
+
+  #get("/dice/1/20", {:controller => "dice", :action => "roll120"})
+
+  get("/dice/:random_dice/:random_sides", {:controller => "dice", :action => "rollrandom"})
+
 end
